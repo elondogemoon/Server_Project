af628a4ac8dc6733d09da5f99ce3dfc78b6ad1e9
amckdgjs <amckdgjs1@gmail.com>
2024-06-04 10:24:58 +0900

    [Commit]프로젝트 초기화 및 에셋 추가

---
+++ Assets/Script/Cards.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cards 
+{
+    public int cardValue;
+
+    public Cards(int cardValue)
+    {
+        this.cardValue = cardValue;
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class Player : NetworkBehaviour
+{
+    public int Chip;
+    public List<Cards> Cards;
+    public List<Player> PlayerList;
+
+    private void Update()
+    {
+        
+    }
+
+
+
+
+}
